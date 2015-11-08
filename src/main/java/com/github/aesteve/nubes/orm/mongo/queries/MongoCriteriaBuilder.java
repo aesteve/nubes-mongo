0cbac232d5f9b5ba29ffcda6fafc10bfc9c68830
Arnaud Esteve <arnaud.esteve@gmail.com>
2015-11-08 18:36:47 +0100

    handle findBy

--- src/main/java/com/github/aesteve/nubes/orm/mongo/queries/MongoCriteriaBuilder.java
+++ src/main/java/com/github/aesteve/nubes/orm/mongo/queries/MongoCriteriaBuilder.java
@@ -2,6 +2,7 @@ package com.github.aesteve.nubes.orm.mongo.queries;
 
 import com.github.aesteve.nubes.orm.queries.FindBy;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 public class MongoCriteriaBuilder<T> {
@@ -15,8 +16,11 @@ public class MongoCriteriaBuilder<T> {
 	
 	public JsonObject toJson() {
 		JsonObject crit = new JsonObject();
-		findBy.getRestrictions().forEach((name, value) -> {
+		findBy.getStrictRestrictions().forEach((name, value) -> {
 			crit.put(name, value);
+		});
+		findBy.getInRestrictions().forEach((name, values) -> {
+			crit.put(name, new JsonObject().put("$in", new JsonArray(values)));
 		});
 		return crit;
 	}
--- src/test/java/unit/crud/CrudSpec.java
+++ src/test/java/unit/crud/CrudSpec.java
@@ -1,8 +1,11 @@
 package unit.crud;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import com.github.aesteve.nubes.orm.queries.FindBy;
+import com.github.aesteve.nubes.orm.queries.ListAndCount;
 
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -20,7 +23,7 @@ public class CrudSpec extends TestBase {
 			Dog result = res.result();
 			context.assertEquals(snowy, result);
 			FindBy<Dog> findBy = new FindBy<>(Dog.class);
-			findBy.addRestriction("name", "Snowy");
+			findBy.eq("name", "Snowy");
 			mongo.findBy(findBy, findRes -> {
 				context.assertFalse(findRes.failed());
 				Dog findResult = findRes.result();
@@ -35,7 +38,7 @@ public class CrudSpec extends TestBase {
 		Async async = context.async();
 		Dog snoop = new Dog("Snoopy", "NoLongerABeagle");
 		FindBy<Dog> crit = new FindBy<>(Dog.class);
-		crit.addRestriction("name", "Snoopy");
+		crit.eq("name", "Snoopy");
 		mongo.update(snoop, crit, res -> {
 			context.assertFalse(res.failed());
 			Dog result = res.result();
@@ -44,6 +47,44 @@ public class CrudSpec extends TestBase {
 				context.assertFalse(findRes.failed());
 				Dog findResult = findRes.result();
 				context.assertEquals(snoop, findResult);
+				async.complete();
+			});
+		});
+	}
+	
+	@Test
+	public void insertAndFindMany(TestContext context) {
+		Async async = context.async();
+		Dog snowy = new Dog("Snowy", "Fox");
+		mongo.create(snowy, res -> {
+			context.assertFalse(res.failed());
+			Dog result = res.result();
+			context.assertEquals(snowy, result);
+			FindBy<Dog> findBy = new FindBy<>(Dog.class);
+			findBy.in("breed", Arrays.asList("Fox", "Beagle"));
+			mongo.listAndCount(findBy, 0, 100, findRes -> {
+				context.assertFalse(findRes.failed());
+				ListAndCount<Dog> findResult = findRes.result();
+				context.assertEquals(2l, findResult.count);
+				async.complete();
+			});
+		});
+	}
+	
+	@Test
+	public void insertAndFindOne(TestContext context) {
+		Async async = context.async();
+		Dog snowy = new Dog("Snowy", "Fox");
+		mongo.create(snowy, res -> {
+			context.assertFalse(res.failed());
+			Dog result = res.result();
+			context.assertEquals(snowy, result);
+			FindBy<Dog> findBy = new FindBy<>(Dog.class);
+			findBy.in("breed", Arrays.asList("Beagle"));
+			mongo.listAndCount(findBy, 0, 100, findRes -> {
+				context.assertFalse(findRes.failed());
+				ListAndCount<Dog> findResult = findRes.result();
+				context.assertEquals(1l, findResult.count);
 				async.complete();
 			});
 		});
